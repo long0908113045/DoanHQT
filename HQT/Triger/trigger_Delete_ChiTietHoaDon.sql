5cb872d0a9d1780595e4ace70fcd47b3c62639b4
long0908113045 <55612567+long0908113045@users.noreply.github.com>
2020-01-02 00:00:14 +0700

    Co Database roi

---
+++ HQT/Triger/trigger_Delete_ChiTietHoaDon.sql
@@ -0,0 +1,16 @@
+﻿IF OBJECT_ID ('trigger_Delete_ChiTietHoaDon') IS NOT NULL
+DROP TRIGGER trigger_Delete_ChiTietHoaDon;
+GO
+CREATE TRIGGER trigger_Delete_ChiTietHoaDon
+ON	ChiTietHoaDon
+FOR Delete
+AS
+BEGIN
+	Declare @MaHD nvarchar(50);
+	Set @MaHD = (Select MaHD from deleted)
+	DECLARE @ThanhTien int ;	
+	Set @ThanhTien = (Select Thanhtien from deleted) 
+	Update HoaDon set TongTien = TongTien-@ThanhTien where MaHD = @MaHD		
+END
+go
+exec SPDeleteChiTietHoaDon 'HD01','AB01'
--- HQT/Triger/trigger_Update_ChiTietHoaDon.sql
+++ HQT/Triger/trigger_Update_ChiTietHoaDon.sql
@@ -7,19 +7,19 @@ FOR Update
 AS
 BEGIN
 	DECLARE @SoLuong int;
-	set @SoLuong =(Select SoLuong from inserted)
+	set @SoLuong =(Select SoLuong from deleted)
 	DECLARE @DonGia int;
-	SET @DonGia = ( Select DonGia from inserted)
+	SET @DonGia = ( Select DonGia from deleted)
 	DECLARE @ThanhTien int ;
 	Set @ThanhTien = @SoLuong*@DonGia
 	DECLARE @MaHD nvarchar(50) ;
-	Set @MaHD = (Select MaHD from inserted)
+	Set @MaHD = (Select MaHD from deleted)
 	DECLARE @MaSP nvarchar(50) ;
-	Set @MaSP = (Select MaSP from inserted)
+	Set @MaSP = (Select MaSP from deleted)
 	declare @thanhTienTruoc int;
 	Set @thanhTienTruoc = (select ThanhTien from ChiTietHoaDon where MaSP =@MaSP and MaHD = @MaHD)
-	Set @ThanhTien = @ThanhTien - @thanhTienTruoc
+	Set @ThanhTien =  @thanhTienTruoc -@ThanhTien
 	Update HoaDon set TongTien = TongTien+@ThanhTien where MaHD = @MaHD		
 END
 go
-exec SPUpdateChiTietHoaDon'HD01','AB01','3','380000'
+exec SPUpdateChiTietHoaDon'HD01','AB01','2','380000'
